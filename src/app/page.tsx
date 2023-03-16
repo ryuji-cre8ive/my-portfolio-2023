b8271d080423dac3bcffb47f3a7d3d4a25e374d5
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2023-03-16 22:07:39 +0900

    :art: format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 export const metadata = {
-  title: 'ryuji-portfolio',
+  title: "ryuji-portfolio",
   twitter: {
-    card: 'summary_large_image',
-    title: 'Ryuji-portfolio',
-    description: 'Portfolio made by ryuji-cre8ive',
-    creator: '@ryuji-cre8ive',
-    images: ['https://ryuji-portfolio-2023.vercel.app/logo.png'],
-  }
-}
+    card: "summary_large_image",
+    title: "Ryuji-portfolio",
+    description: "Portfolio made by ryuji-cre8ive",
+    creator: "@ryuji-cre8ive",
+    images: ["https://ryuji-portfolio-2023.vercel.app/logo.png"],
+  },
+};
 
 export default function Home() {
   return (
