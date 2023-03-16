cc92846cb3f83bcd0c4a604857d86f396d9df66f
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2023-03-16 22:26:55 +0900

    :art: ogp for twitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
 import "./globals.css";
 import Header from "./components/Header";
 
+export const metadata = {
+  title: "ryuji-portfolio",
+  twitter: {
+    card: "summary_large_image",
+    title: "Ryuji-portfolio",
+    description: "Portfolio made by ryuji-cre8ive",
+    creator: "@ryuji-cre8ive",
+    images: ["https://ryuji-portfolio-2023.vercel.app/logo.png"],
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
